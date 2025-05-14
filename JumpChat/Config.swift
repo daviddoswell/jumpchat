92ddc85b59d1b271adfed2682d94854e365d7242
daviddoswell <111773826+daviddoswell@users.noreply.github.com>
2025-05-14 02:55:05 -0600

    Remove Config.swift from tracking

--- JumpChat/Config.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-enum Config {
-    static let openAIKey = "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"
-    static let elevenLabsApiKey = "sk_b083648daee3fa6cd371c151f528b3973514e9e624be9194"
-    static let elevenLabsVoiceId = "L0Dsvb3SLTyegXwtm47J"
-    static let elevenLabsAgentId = "qXo28jT0r9zxUIq33Bjo"
-}
